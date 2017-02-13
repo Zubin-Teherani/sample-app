8446d98e5401c925643be666e3b487fd757932f7
Zubin Teherani <zubin.teherani@gmail.com>
2017-02-13 09:05:29 -0500

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    # User strongly typed params
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+    # Define strongly typed params
+    def user_params
+      params.require(:user).permit(:name,
+                                   :email,
+                                   :password,
+                                   :password_confirmation
+                                  )
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
   get 'about', to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
   get 'signup', to: 'users#new'
+
+  # Users resource
+  post '/signup', to: 'users#create'
+  resources :users
 end
